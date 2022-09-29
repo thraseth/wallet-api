68a0c22a1adcd205e75cd014ff94099a2c6910c4
thras.eth <98333789+thraseth@users.noreply.github.com>
2022-09-29 23:43:35 +0200

    Update Balances.service.ts
    This commit includes:
    
    * A more functional programming 
    * Use of .map method instead of a FOR EACH. Fixed using the Promise.all method.
    * Some other slight type optimizations

--- src/Balances.service.ts
+++ src/Balances.service.ts
@@ -29,25 +29,20 @@ const setARB = { //arbitrum
   network: Network.ARB_MAINNET,
 };
 
+let alchemy : Alchemy;
+let chainID : PLATFORMS;
 
 @Injectable()
 export class BalancesService {
 
   /**
    * 
-   * Retrieves the non-null list of erc20 tokens in a specified chain & wallet address
+   * Sets up the Alchemy connection
    * 
-   * @param chain - The desired allowed chain 
-   * @param wallet - The wallet address we try to retrieve the tokens from
-   * 
-   * @returns The erc20 list or throws an error
+   * @param chain - The selected chain
    * 
    */
-  async getWalletContent(chain :allowedChain, wallet :string) :Promise<string | token[]> {
-    let alchemy : Alchemy;
-    let chainID : PLATFORMS;
-
-    //Sets up the chain connection
+  setUpChain (chain: allowedChain) {
     switch (chain) {
       case allowedChain.eth:
         alchemy = new Alchemy(setETH);
@@ -61,8 +56,19 @@ export class BalancesService {
         alchemy = new Alchemy(setARB);
         chainID ='arbitrum-one';
     }
+  }
 
 
+  /**
+   * 
+   * Retrieves the @wallet 's data from Alchemy and keeps only the positive balances
+   * 
+   * @param wallet - The selected wallet
+   * 
+   * @returns an array of objects as a promise 
+   *
+   */
+  async fetchFilteredData (wallet :string) : Promise<[]> {
     //Tries to retrieve the token balances for the wallet parameter
     let result;
     try{
@@ -73,18 +79,28 @@ export class BalancesService {
       throw new Error (error.body);
     }
 
-
     //Filters the resulting list to get only balances > 0
     const filtered = result.tokenBalances.filter((token) => {return token.tokenBalance !== '0' && Number(token.tokenBalance) != 0 });
 
-    //Creates an array of tokens
-    let list: Array<token> = [];
+    return filtered;
+  }
 
-    //Loops through every retrieved erc20
-    for(const t of filtered){
-      let balance_usd: number | string;
-      let tokenData;
-      let newToken:token;
+
+  /**
+   * 
+   * Formates the objects from the filtered array to tokens using the token interface
+   * 
+   * @param filteredList 
+   *
+   * @returns our array of tokens as a promise
+   *
+   */
+   async reformateList ( filteredList: Array<any>) : Promise<token[]>{
+    //reformates our filtered list
+    return Promise.all(filteredList.map(async (t) => {
+      let balance_usd :string;
+      let tokenData :any;
+      let newToken :token;
 
       //Retrieves the token metadata from alchemy
       let metadata = await alchemy.core.getTokenMetadata(t.contractAddress)
@@ -92,16 +108,15 @@ export class BalancesService {
       //Retrieves the token informations from coingecko API
       tokenData = await client.contract({id :chainID, contract_address :t.contractAddress});
 
-
       if (tokenData.error){ //It means that the current token is not present in coingecko's database - sets the balance_usd as unretrievable
         balance_usd = "unretrievable";
       }
       else{ //If the token exists in coingecko's database, sets balance_usd according to the current USD price
-        balance_usd= (tokenData.market_data.current_price.usd * (Number(t.tokenBalance) / Math.pow(10, metadata.decimals))).toFixed(2);
+        balance_usd= (tokenData.market_data.current_price.usd * (Number(t.tokenBalance) / Math.pow(10, metadata.decimals))).toFixed(2) + "usd";
       }
 
       //Serializes the token data and pushes the new token object in our array
-      newToken= {
+      return newToken = {
         token_address: t.contractAddress,
         name: metadata.name,
         symbol: metadata.symbol,
@@ -109,11 +124,29 @@ export class BalancesService {
         balance: Number(t.tokenBalance).toString(),
         balance_usd: balance_usd
       }
-      list.push(newToken);
-    }
-
-    //Returns the array of token objects
-    return list;
+    }))
   }
+
+  /**
+   * 
+   * Retrieves the non-null list of erc20 tokens in a specified chain & wallet address
+   * 
+   * @param chain - The desired allowed chain 
+   * @param wallet - The wallet address we try to retrieve the tokens from
+   * 
+   * @returns The erc20 list or throws an error
+   * 
+   */
+     async getWalletContent(chain :allowedChain, wallet :string) :Promise<token[]> {
+    
+      //Sets up the chain connection
+      this.setUpChain(chain);
+  
+      //Tries to retrieve the erc20 tokens with a positive balance from @wallet
+      let filteredList = await this.fetchFilteredData(wallet);
+      
+      //Returns the array of serialized token objects
+      return this.reformateList(filteredList);
+    }
 
 }
